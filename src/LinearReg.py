cfbd624fc30b6c4631a913af1aa4fbd9ff03eb02
Kaijun He <hekaijun1017@gmail.com>
2019-11-11 23:42:04 -0800

    Fixed some bugs.

--- src/LinearReg.py
+++ src/LinearReg.py
@@ -13,14 +13,18 @@ def main(path, input_col, label_col, data_start, data_end):
         train_path: Path to CSV file containing dataset for training.
     """
     x, y = util.load_dataset(path, input_col, label_col)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x[data_start: data_end], y[data_start: data_end], test_size=0.33, random_state=0)
     n_label = len(label_col)
     n_input = len(input_col)
     theta = np.zeros((n_label, n_input + 1))
     for i in range(n_label):
         reg = LinearRegression().fit(x_train, y_train[:, 0])
         theta[i, 0] = reg.intercept_
+        print(reg.coef_)
         theta[i, 1:] = reg.coef_
+
+        print('Multiple Linear Regression Score : ', reg.score(x_test, y_test[:, i]))
+
     if n_input == 1:
         for i in range(n_label):
             save_path = "plots/" + input_col[0] + '_vs_' + label_col[i] + '.png'
@@ -31,7 +35,7 @@ def main(path, input_col, label_col, data_start, data_end):
 
 if __name__ == '__main__':
     main(path='pmsm_temperature_data.csv',
-         input_col=["u_d"],
+         input_col=["pm", "coolant", "ambient", "i_d", "u_d"],
          label_col=["stator_yoke", "stator_tooth", "stator_winding"],
          data_start=0,
          data_end=1000)
