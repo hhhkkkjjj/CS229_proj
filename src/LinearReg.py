c5c7558fdb5f1487fcb631667b86b0f6cef836e7
Kaijun He <hekaijun1017@gmail.com>
2019-11-14 16:19:19 -0800

    Add ransac

--- src/LinearReg.py
+++ src/LinearReg.py
@@ -2,9 +2,10 @@ import numpy as np
 import util
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import RANSACRegressor
 import pandas as pd
 
-def main(profile, input_col, label_col, cross, profile_test):
+def main(profile, input_col, label_col, ransac, cross, profile_test):
     """
     :param profile: read profile_id, int type
     :param input_col: list of X name
@@ -35,25 +36,29 @@ def main(profile, input_col, label_col, cross, profile_test):
     high_score=0
     for i in range(n_label):
         print('For label',label_col[i]," : ")
-        reg = LinearRegression().fit(x_train, y_train[:, i])
-        theta[i, 0] = reg.intercept_
-        # print(reg.coef_)
-        theta[i, 1:] = reg.coef_
+        if not ransac:
+            reg = LinearRegression().fit(x_train, y_train[:, i])
+            theta[i, 0] = reg.intercept_
+            theta[i, 1:] = reg.coef_
+        else:
+            reg = RANSACRegressor().fit(x_train, y_train[:, i])
+            theta[i, 0] = reg.estimator_.intercept_
+            theta[i, 1:] = reg.estimator_.coef_
 
-        score=reg.score(x_test, y_test[:, i])
-        if score>high_score:
-            high_score=score
-            label=label_col[i]
+        score = reg.score(x_test, y_test[:, i])
+        if score > high_score:
+            high_score = score
+            label = label_col[i]
         x_new = util.add_intercept(x_test)
         pred = x_new.dot(theta[i, :])
         den = pd.DataFrame({'Actual': y_test[:, i],
                             'Prediction': pred, })
         p = den.plot.kde()
         fig = p.get_figure()
-        fig.savefig("density_plot/" + label + '_density.png')
-        print('Multiple Linear Regression Score for',label,'is',
+        fig.savefig("density_plot/" + label_col[i] + '_density.png')
+        print('Multiple Linear Regression Score for', label_col[i], 'is',
               reg.score(x_test, y_test[:, i]))
-    print('In profile', profile, 'the highest score for',label,'is',high_score)
+    print('In profile', profile, 'the highest score for', label, 'is',high_score)
 
     # if only one input, we can plot it
     if n_input == 1:
@@ -67,11 +72,12 @@ def main(profile, input_col, label_col, cross, profile_test):
 
 
 if __name__ == '__main__':
-    input_col = ["coolant", "i_d", "i_q", "u_d", "u_q"]
+    input_col = ["ambient", "coolant", "motor_speed", "i_d", "i_q", "u_d", "u_q", "i", "u", "Power"]
     label_col = ["pm","stator_yoke", "stator_tooth", "stator_winding"]
     path = 'profile_data/Profile_4.csv'
     main(profile=4,
         input_col=input_col,
         label_col=label_col,
+        ransac=True,
         cross = False,  # cross test: True
         profile_test=6) # if cross test, profile used for test
